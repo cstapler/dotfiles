5f8d70203fd88a23c831030f33287c20b64e69a1
Chris Stapler <cstapler@iastate.edu>
2017-07-31 14:43:36 -0400

    Add script to install bash-it for use on machines without sudo access

---
+++ fallback.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#When Zsh is not avaiable use use bash-it instead
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sha
+echo -e "Enter \e[1;31m\`source ~/.bashrc\`\e[0m to finish setup."
